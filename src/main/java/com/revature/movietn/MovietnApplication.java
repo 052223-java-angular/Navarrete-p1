66a88d4f3fd4cca865ad91f9515b42438682983e
Thomas Navarrete <78869048+TNavarrete1@users.noreply.github.com>
2023-06-04 21:27:45 -0700

    Setup project structure and dependencies

---
+++ src/main/java/com/revature/movietn/MovietnApplication.java
@@ -0,0 +1,13 @@
+package com.revature.movietn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovietnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovietnApplication.class, args);
+    }
+
+}
