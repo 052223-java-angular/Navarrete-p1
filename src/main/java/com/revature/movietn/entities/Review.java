4643b56980c2eafdcb905276b2e4ff6cc4e26b24
Thomas Navarrete <78869048+TNavarrete1@users.noreply.github.com>
2023-06-05 21:55:17 -0700

    Create entities based off of erd

---
+++ src/main/java/com/revature/movietn/entities/Review.java
@@ -0,0 +1,51 @@
+package com.revature.movietn.entities;
+
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "reviews")
+public class Review {
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    private int rating;
+
+    @Column(nullable = false)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    @JsonBackReference
+    private Movie movie;
+
+    public Review(int rating, String description, User user, Movie movie) {
+        this.id = UUID.randomUUID().toString();
+        this.rating = rating;
+        this.description = description;
+        this.user = user;
+        this.movie = movie;
+    }
+}
