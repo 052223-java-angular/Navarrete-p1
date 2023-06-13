f40cad2f5a7d033dfff4d4258d9b35c52f47c22c
Thomas Navarrete <78869048+TNavarrete1@users.noreply.github.com>
2023-06-12 21:20:01 -0700

    Add cross origin to controllers

--- src/main/java/com/revature/movietn/controllers/AuthController.java
+++ src/main/java/com/revature/movietn/controllers/AuthController.java
@@ -2,6 +2,7 @@ package com.revature.movietn.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin
 public class AuthController {
     private final UserService userService;
     private final JwtTokenService jwtTokenService;
